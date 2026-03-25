a983b24d09f4d0cec0ad014aed5c68552701f51b
verat <veratibucia@gmail.com>
2026-03-25 13:06:40 +0100

    q

--- lab1/lib/pages/main_view.dart
+++ lab1/lib/pages/main_view.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:lab1/pages/add_view.dart';
+import 'package:lab1/widgets/task_list.dart';
 
 class MainView extends StatelessWidget {
   const MainView({super.key});
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Things todo'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('DAT216/TIG091'),
-            Text('Lab1 Todos'),
-          ],
-        ),
-      ),
-    );
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          ),
+      body: TaskList(),
+        floatingActionButton: FloatingActionButton(
+        onPressed: () {
+             Navigator.push(
+                 context,
+                 MaterialPageRoute(
+                       builder: (context) => AddView(),
+                 )
+             );
+        },
+        toolTip: 'Add todo',
+        child: const Icon(Icons.add),
+        ));
   }
 }
